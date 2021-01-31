10b414e4027e9db28d9b3588569ed5dbe4a3ef23
daveymclain <braveheart_52@hotmail.com>
2021-01-31 21:06:22 +0000

    Added Add screen

--- main.py
+++ main.py
@@ -10,15 +10,22 @@ pygame.display.set_caption('Play Aid!')
 window = pygame.display.set_mode(WINDOW_DIMENSIONS, 0, 32)
 
 
-def event_manager(click, release):
+def event_manager(click, release, sub_menu=False):
+    run = True
     for event in pygame.event.get():
         if event.type == QUIT:
-            pygame.quit()
-            sys.exit()
-        if event.type == KEYDOWN:
-            if event.key == K_ESCAPE:
+            if not sub_menu:
                 pygame.quit()
                 sys.exit()
+            else:
+                run = False
+        if event.type == KEYDOWN:
+            if event.key == K_ESCAPE:
+                if not sub_menu:
+                    pygame.quit()
+                    sys.exit()
+                else:
+                    run = False
         if event.type == MOUSEBUTTONDOWN:
             if event.button == 1:
                 click = True
@@ -30,23 +37,33 @@ def event_manager(click, release):
                 click = False
         else:
             release = False
-    return click, release
+    return click, release, run
 
 
 def main_menu():
-    add_button = gui.Button(window, name="Add Toy", pos=(10, 10))
+    add_button = gui.Button(window, name="New Toy", pos=(10, 10))
     click, release = False, False
     while True:
+        window.fill((0, 0, 0))
         add_button.collide_point(pygame.mouse.get_pos())
-
         add_button.draw(click)
         pygame.display.update()
         main_clock.tick(60)
-        click, release = event_manager(click, release)
+        click, release, run = event_manager(click, release)
         if add_button.mouse_over and release:
-            print("go to new menu")
             release = False
+            new_toy_menu()
 
 
+def new_toy_menu():
+    run = True
+    click, release = False, False
+    while run:
+        window.fill((255, 255, 255))
+        pygame.display.update()
+        main_clock.tick(60)
+        click, release, run = event_manager(click, release, sub_menu=True)
 
-main_menu()
+
+if __name__ == "__main__":
+    main_menu()
