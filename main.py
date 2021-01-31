7efe96e9abb95e3c14cbd6a332afa4cf80c0473b
daveymclain <braveheart_52@hotmail.com>
2021-01-31 14:28:45 +0000

    Added button dim when clicked

--- main.py
+++ main.py
@@ -10,22 +10,43 @@ pygame.display.set_caption('Play Aid!')
 window = pygame.display.set_mode(WINDOW_DIMENSIONS, 0, 32)
 
 
-def main_menu():
-    add_button = gui.Button(window, name="Add Toy", pos=(10, 10))
-    while True:
-        add_button.draw()
-        add_button.collide_point(pygame.mouse.get_pos())
-        for event in pygame.event.get():
-            if event.type == QUIT:
+def event_manager(click, release):
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+        if event.type == KEYDOWN:
+            if event.key == K_ESCAPE:
                 pygame.quit()
                 sys.exit()
-            if event.type == KEYDOWN:
-                if event.key == K_ESCAPE:
-                    pygame.quit()
-                    sys.exit()
+        if event.type == MOUSEBUTTONDOWN:
+            if event.button == 1:
+                click = True
+            else:
+                click = False
+        if event.type == MOUSEBUTTONUP:
+            if event.button == 1:
+                release = True
+                click = False
+        else:
+            release = False
+    return click, release
 
+
+def main_menu():
+    add_button = gui.Button(window, name="Add Toy", pos=(10, 10))
+    click, release = False, False
+    while True:
+        add_button.collide_point(pygame.mouse.get_pos())
+
+        add_button.draw(click)
         pygame.display.update()
         main_clock.tick(60)
+        click, release = event_manager(click, release)
+        if add_button.mouse_over and release:
+            print("go to new menu")
+            release = False
+
 
 
 main_menu()
