57dae69e17bef3bffe5046281b2ec7ec04afda8f
daveymclain <braveheart_52@hotmail.com>
2021-02-02 11:23:13 +0000

    Working on event manager

--- main.py
+++ main.py
@@ -11,9 +11,9 @@ window = pygame.display.set_mode(WINDOW_DIMENSIONS, 0, 32)
 font = pygame.font.SysFont("comicsansms", 72)
 
 
-def event_manager(click, release, sub_menu=False):
+def click_manager(click, release, events, sub_menu=False):
     run = True
-    for event in pygame.event.get():
+    for event in events:
         if event.type == QUIT:
             if not sub_menu:
                 pygame.quit()
@@ -50,7 +50,8 @@ def main_menu():
         add_button.draw(click)
         pygame.display.update()
         main_clock.tick(60)
-        click, release, run = event_manager(click, release)
+        event_list = pygame.event.get()
+        click, release, run = click_manager(click, release, event_list)
         if add_button.mouse_over and release:
             release = False
             new_toy_menu()
@@ -62,16 +63,14 @@ def new_toy_menu():
     text_input_box = gui.TextInputBox(50, 50, 400, font)
     group = pygame.sprite.Group(text_input_box)
     while run:
-        window.fill((255, 255, 255))
-        # click, release, run = event_manager(click, release, sub_menu=True)
-        main_clock.tick(60)
+        window.fill((0, 0, 0))
         event_list = pygame.event.get()
+        click, release, run = click_manager(click, release, event_list, sub_menu=True)
         for event in event_list:
             if event.type == pygame.QUIT:
                 run = False
         group.update(event_list)
-
-        window.fill(0)
+        main_clock.tick(60)
         group.draw(window)
         pygame.display.flip()
 
