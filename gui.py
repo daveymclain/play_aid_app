2cb35193d670005c0397e9b01df5f069651ac84d
daveymclain <braveheart_52@hotmail.com>
2021-02-05 22:18:22 +0000

    separated some of the functions and enhanced the click to move the cursor

--- gui.py
+++ gui.py
@@ -1,4 +1,4 @@
-import pygame, time
+import pygame, time, math
 
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
@@ -49,55 +49,52 @@ class TextInputBox(pygame.sprite.Sprite):
         self.text_widths = {"current": 0}
         self.t_surf = None
         self.cursor_index = -1
-
+        self.margin = 0
+        # render default text
         self.render_text()
 
-    def store_text_width(self, character=""):
+    def update_t_surf(self, character=""):
+        """Updates the text surface with the self.text and add to a dictionary of width for the individual characters"""
         self.t_surf = self.font.render(self.text, True, self.text_colour, self.backcolor)
         if character:
             self.text_widths[character] = self.t_surf.get_width() - self.text_widths["current"]
-
         self.text_widths["current"] = self.t_surf.get_width()
 
-    def render_text(self):
-        if not self.text and not self.active:
-            self.t_surf = self.font.render(self.default_text, True, self.text_colour, self.backcolor)
+    def find_cursor_pos(self):
+        """Convert the cursor index into x position for the cursor"""
+        running_width = 0
+        if self.cursor_index == -2:
+            return 0
+        for ind, character in enumerate(self.text):
+            running_width += self.text_widths[character]
+            if ind == self.cursor_index:
+                return running_width
 
-        margin = int(self.t_surf.get_height() * 0.1)
-        if self.cursor_index == -1:
-            curs_pos_x = self.text_widths["current"] + 5
-        else:
-            running_width = 0
-            for ind, character in enumerate(self.text):
-                running_width += self.text_widths[character]
-                if ind == self.cursor_index:
-                    curs_pos_x = running_width + 5
+    def find_the_index(self, mouse_pos_x):
+        """Find the cursor index when clicking within the text field"""
+        mouse_pos_x = mouse_pos_x - self.pos[0] - 5
+        running_width = 0
+        for ind, character in enumerate(self.text):
+            current_ind_width = round(self.text_widths[character] / 2)
+            if current_ind_width > mouse_pos_x and ind == 0:
+                self.cursor_index = -2
+                break
+            running_width += self.text_widths[character]
+            if mouse_pos_x < running_width:
+                if mouse_pos_x < running_width - current_ind_width:
+                    self.cursor_index = ind - 1
                     break
-        self.image = pygame.Surface((max(self.width, self.text_widths["current"] + margin * 2),
-                                     self.t_surf.get_height() + margin),
-                                    pygame.SRCALPHA)
-        if self.backcolor:
-            self.image.fill(self.backcolor)
-        self.image.blit(self.t_surf, (5, 0))
-        if self.active:
-            outline_colour = WHITE
-            if self.flash:
-                pygame.draw.rect(self.image, WHITE, pygame.Rect((curs_pos_x, int(margin * 2)),
-                                                                (2, self.image.get_height() - int(margin * 4))))
+                if mouse_pos_x > running_width - current_ind_width:
+                    self.cursor_index = ind
+                    break
             else:
-                pygame.draw.rect(self.image, GREY, pygame.Rect((curs_pos_x, int(margin * 2)),
-                                                               (2, self.image.get_height() - int(margin * 4))))
-        else:
-            outline_colour = GREY
-        pygame.draw.rect(self.image, outline_colour, self.image.get_rect().inflate(-2, -2), 2)
+                self.cursor_index = -1
+        if len(self.text) == self.cursor_index + 1:
+            self.cursor_index = -1
 
-        self.rect = self.image.get_rect(topleft=self.pos)
-
-    def update(self, event_list):
-        if time.time() - self.backspace["pressed_start_time"] > 0.5 and self.backspace["pressed"]:
-            self.text = self.text[:-1]
-            self.store_text_width()
-        if self.tick > 40:
+    def cursor_blink(self, speed):
+        """Works out when to blink the cursor based on speed(the amount of frames)"""
+        if self.tick > speed:
             self.tick = 0
             if self.flash:
                 self.flash = False
@@ -105,48 +102,102 @@ class TextInputBox(pygame.sprite.Sprite):
                 self.flash = True
         else:
             self.tick += 1
+
+    def backspace_cont_press(self, delay):
+        """When the backspace key is pressed for longer than the delay. delete from self.text once a frame"""
+        if time.time() - self.backspace["pressed_start_time"] > delay and self.backspace["pressed"]:
+
+            if self.cursor_index == -1:
+                self.text = self.text[:-1]
+            elif self.cursor_index == -2:
+                return
+            else:
+                first_half = self.text[:self.cursor_index]
+                second_half = self.text[self.cursor_index + 1:]
+                self.text = first_half + second_half
+                self.cursor_index -= 1
+                if self.cursor_index == -1:
+                    self.cursor_index = -2
+        self.update_t_surf()
+
+
+    def render_text(self):
+        if not self.text and not self.active:
+            self.t_surf = self.font.render(self.default_text, True, self.text_colour, self.backcolor)
+        self.margin = int(self.t_surf.get_height() * 0.1)
+        if self.cursor_index == -1:
+            curs_pos_x = self.text_widths["current"] + 5
+        else:
+            curs_pos_x = self.find_cursor_pos() + 5
+        self.image = pygame.Surface((max(self.width, self.text_widths["current"] + self.margin * 2),
+                                     self.t_surf.get_height() + self.margin),
+                                    pygame.SRCALPHA)
+        if self.backcolor:
+            self.image.fill(self.backcolor)
+        self.image.blit(self.t_surf, (5, 0))
+        if self.active:
+            outline_colour = WHITE
+            if self.flash:
+                pygame.draw.rect(self.image, WHITE, pygame.Rect((curs_pos_x, int(self.margin * 2)),
+                                                                (2, self.image.get_height() - int(self.margin * 4))))
+            else:
+                pygame.draw.rect(self.image, GREY, pygame.Rect((curs_pos_x, int(self.margin * 2)),
+                                                               (2, self.image.get_height() - int(self.margin * 4))))
+        else:
+            outline_colour = GREY
+        pygame.draw.rect(self.image, outline_colour, self.image.get_rect().inflate(-2, -2), 2)
+
+        self.rect = self.image.get_rect(topleft=self.pos)
+
+    def update(self, event_list):
+        self.cursor_blink(40)
+        self.backspace_cont_press(0.5)
+
         for event in event_list:
             if event.type == pygame.MOUSEBUTTONDOWN:
                 self.active = bool(self.rect.collidepoint(event.pos))
-                self.store_text_width()
+                self.update_t_surf()
                 self.render_text()
                 if self.active:
                     mouse_pos_x = event.pos[0]
-                    running_width = 0
-                    for ind, character in enumerate(self.text):
-                        running_width += self.text_widths[character]
-                        if mouse_pos_x > running_width:
-                            self.cursor_index = ind
-                            print(f"index {self.cursor_index}")
-                            if len(self.text) == self.cursor_index + 1:
-                                self.cursor_index = -1
-                            # break
+                    self.find_the_index(mouse_pos_x)
             if event.type == pygame.KEYUP:
                 if event.key == pygame.K_BACKSPACE:
                     self.backspace["pressed"] = False
             if event.type == pygame.KEYDOWN and self.active:
                 if event.key == pygame.K_RETURN:
                     self.active = False
-                    self.store_text_width()
+                    self.update_t_surf()
                     self.render_text()
                 elif event.key == pygame.K_BACKSPACE:
+                    if self.cursor_index == -2:
+                        continue
                     self.backspace["pressed_start_time"] = time.time()
                     self.backspace["pressed"] = True
-                    self.text = self.text[:-1]
-                    self.store_text_width()
+
                     if self.cursor_index != -1:
+                        first_half = self.text[:self.cursor_index ]
+                        second_half = self.text[self.cursor_index + 1:]
+                        self.text = first_half + second_half
                         self.cursor_index -= 1
+                        if self.cursor_index == -1:
+                            print("set cursor to start pos")
+                            self.cursor_index = -2
+                    else:
+                        self.text = self.text[:-1]
+                    self.update_t_surf()
             if event.type == pygame.TEXTINPUT and self.active:
                 if self.cursor_index == -1:
                     self.text += event.text
-                    self.store_text_width(event.text)
+                elif self.cursor_index == -2:
+                    self.text = event.text + self.text
                 else:
-                    first_half = self.text[:self.cursor_index]
-                    second_half = self.text[self.cursor_index:]
+                    first_half = self.text[:self.cursor_index + 1]
+                    second_half = self.text[self.cursor_index + 1:]
                     first_half += event.text
                     self.cursor_index += 1
                     self.text = first_half + second_half
-                    self.store_text_width(event.text)
+                self.update_t_surf(event.text)
 
         if self.active:
             self.render_text()
